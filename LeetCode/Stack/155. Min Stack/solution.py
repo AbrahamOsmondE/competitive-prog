d2dc555b8075ea1fb05abfb742d7e5a8b41547e6
abrahamosmonde <abraham.osmond@gmail.com>
2024-08-01 00:06:23 +0800

    min stack solution

---
+++ LeetCode/Stack/155. Min Stack/solution.py
@@ -0,0 +1,36 @@
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        if (len(self.stack) == 0):
+            self.stack.append([val, val])
+        else:
+            self.stack.append([val, min(self.stack[-1][-1], val)])
+        """
+        :type val: int
+        :rtype: None
+        """
+        
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+        
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][-1]
+        
