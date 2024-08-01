3c7aa597bc57322605b2bc4a0403c8b939393e00
abrahamosmonde <abraham.osmond@gmail.com>
2024-08-01 23:03:11 +0800

    improved solution on temperature

--- LeetCode/Stack/739. Daily Temperatures/solution.py
+++ LeetCode/Stack/739. Daily Temperatures/solution.py
@@ -13,3 +13,28 @@ class Solution(object):
             
             stack.append([temp, i])
         return ans
+    
+# O(1) Memory solution
+    
+class Solution(object):
+    def dailyTemperatures(self, temperatures):
+        """
+        :type temperatures: List[int]
+        :rtype: List[int]
+        """
+        n = len(temperatures)
+        ans = [0] * n
+        hottest = 0
+
+        for i in range(n - 1, -1, -1):
+            temp = temperatures[i]
+            if temp >= hottest:
+                hottest = temp
+                continue
+            
+            days = 1
+            while temperatures[i + days] <= temp:
+                days += ans[i + days]
+            
+            ans[i] = days
+        return ans
